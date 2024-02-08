3a1138acf8ba6259d0e08804c7a356dda0fd5693
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2024-02-08 05:39:42 +0000

    code updated

--- Website_Availability_Checker.sh
+++ Website_Availability_Checker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-# Set the website URL to check
-website_url="google.com"
+# Set the website URL to check (include http:// or https://)
+website_url="https://gmail.com"
 
 # Perform a simple HTTP request and check the status code
-response=$(curl -Is "$website_url" | head -n 1 | cut -d ' ' -f2)
+response=$(curl -Isk "$website_url" | head -n 1 | cut -d ' ' -f2)
 
-if [ "$response" == "200" ]; then
+if [ "$response" == "200" ] || [ "$response" == "301" ] || [ "$response" == "302" ]; then
     echo "Website is accessible."
 else
     echo "Website is not accessible. Status code: $response"
